a78f7e1665569d9d27dd9e432b67296d3d1c2b90
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2021-09-13 14:12:31 +0200

    Add a way to reproduce the error

---
+++ test/test_eigenvals.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.sparse.linalg import eigsh, ArpackNoConvergence
+from scipy.linalg import eigh, eig
+
+def _eigh(a, b, dim, i=0):
+  if i%3 == 0:
+    try:
+      return eigsh(a, k=dim, M=b, which='LA')
+    except np.linalg.LinAlgError:
+      pass  # scipy already tried eigh for us
+    except (ValueError, ArpackNoConvergence):
+      pass
+  elif i%3 == 1:
+    try:
+      return eigh(a, b)
+    except np.linalg.LinAlgError:
+      pass
+  else:
+    return eig(a, b)
+
+def find_error_eigenvalues():
+  attempt = 1
+  while True:
+    a = np.random.rand(2,2)
+    # Symemtrice
+    a = (a + a.T) / 2
+    
+    prev = []
+    n = set()
+    for i in range(10000):
+      vals, vecs = _eigh(a, np.identity(np.shape(a)[0]), np.shape(a)[0], i)
+      for v in vecs:
+        if v[0] not in prev: # First value of an eigenvector
+          prev.append(v[0])
+          _ = [n.add(x) for x in vals]
+          #print(f'New vals: {val}')
+    if len(set(prev)) % np.shape(a)[0] > 0:
+      print(f'Found {len(n)} eigenvalues{n}')
+      print(f'First {len(set(prev))} values of vectors found {prev}')
+      print(f'Aattempts: {attempt}')
+      return
+    attempt += 1
+
+find_error_eigenvalues()
